2a4ffffbdadbdb01ce091b876044cff8aa930b10
Your Name <you@example.com>
2022-12-25 12:20:35 +0200

    added statefull widged

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(MaterialApp(
@@ -6,18 +7,51 @@ void main() {
       backgroundColor: Colors.teal,
       appBar: AppBar(
         title: const Text('Dice'),
+        centerTitle: true,
         backgroundColor: Colors.teal,
       ),
-      body: DicePage(),
+      body: const DiceApp(),
     ),
   ));
 }
 
-class DicePage extends StatelessWidget {
-  const DicePage({super.key});
+class DiceApp extends StatefulWidget {
+  const DiceApp({super.key});
+
+  @override
+  State<DiceApp> createState() => _DiceAppState();
+}
+
+class _DiceAppState extends State<DiceApp> {
+  int leftDiceNumber = Random().nextInt(6) + 1;
+  int rightDiceNumber = Random().nextInt(6) + 1;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: TextButton(
+                  onPressed: () {
+                    changeDiceNumber();
+                  },
+                  child: Image.asset('images/dice$leftDiceNumber.png'))),
+          Expanded(
+              child: TextButton(
+                  onPressed: () {
+                    changeDiceNumber();
+                  },
+                  child: Image.asset('images/dice$rightDiceNumber.png'))),
+        ],
+      ),
+    );
+  }
+
+  void changeDiceNumber() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
   }
 }
